f0475ffa43b981edd8314ae34480ec77f300f295
Topy <gmsdl1994@naver.com>
2024-05-31 20:20:49 +0900

    analyzer 모듈 추가

---
+++ module-analyzer/src/main/java/analyzer/ModuleAnalyzerApplication.java
@@ -0,0 +1,13 @@
+package analyzer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ModuleAnalyzerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ModuleAnalyzerApplication.class, args);
+    }
+
+}
---
+++ module-analyzer/src/test/java/analyzer/ModuleAnalyzerApplicationTests.java
@@ -0,0 +1,13 @@
+package analyzer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ModuleAnalyzerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
