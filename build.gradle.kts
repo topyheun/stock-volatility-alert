388dca8ed58cd0cbc2f674f2791a6276abecf56e
Topy <gmsdl1994@naver.com>
2024-06-02 15:26:34 +0900

    루트 빌드 파일의 플러그인 구문 apply false 옵션 추가

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
     java
-    id("org.springframework.boot") version "3.3.0"
-    id("io.spring.dependency-management") version "1.1.5"
+    id("org.springframework.boot") version "3.3.0" apply false
+    id("io.spring.dependency-management") version "1.1.5" apply false
 }
 
 allprojects {
