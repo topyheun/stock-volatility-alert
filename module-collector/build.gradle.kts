0271e9ca1067895d41728604531d54154815ada1
Topy <gmsdl1994@naver.com>
2024-06-03 17:57:14 +0900

    webclient 설정 추가

--- module-collector/build.gradle.kts
+++ module-collector/build.gradle.kts
@@ -1,5 +1,7 @@
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("org.springframework.kafka:spring-kafka")
+    implementation("org.springframework.boot:spring-boot-starter-webflux")
+
     testImplementation("org.springframework.kafka:spring-kafka-test")
 }
---
+++ module-collector/src/main/java/collector/global/config/WebClientConfig.java
@@ -0,0 +1,14 @@
+package collector.global.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.reactive.function.client.WebClient;
+
+@Configuration
+public class WebClientConfig {
+
+    @Bean
+    public WebClient webClient(WebClient.Builder builder) {
+        return builder.baseUrl("https://api.upbit.com/v1").build();
+    }
+}
