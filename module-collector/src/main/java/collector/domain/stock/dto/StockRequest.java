2a1d46040d4f3affca5bedb7b59e4f27ee10c17a
Topy <gmsdl1994@naver.com>
2024-06-03 18:02:51 +0900

    종목 정보 수집 dto에 json 필드 매핑 적용

--- module-collector/src/main/java/collector/domain/stock/dto/StockRequest.java
+++ module-collector/src/main/java/collector/domain/stock/dto/StockRequest.java
@@ -1,7 +1,11 @@
 package collector.domain.stock.dto;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 public record StockRequest(
     String market,
+
+    @JsonProperty("trade_price")
     int tradePrice
 ) {
 
