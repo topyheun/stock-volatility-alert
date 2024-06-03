f613e4ed73e03d539e973303ab2b310229855900
Topy <gmsdl1994@naver.com>
2024-06-03 18:00:44 +0900

    종목 정보 수집 기능 추가

--- module-collector/src/main/java/collector/ModuleCollectorApplication.java
+++ module-collector/src/main/java/collector/ModuleCollectorApplication.java
@@ -2,12 +2,14 @@ package collector;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class ModuleCollectorApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ModuleCollectorApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ModuleCollectorApplication.class, args);
+    }
 
 }
---
+++ module-collector/src/main/java/collector/domain/stock/application/StockCollectService.java
@@ -0,0 +1,35 @@
+package collector.domain.stock.application;
+
+import collector.domain.stock.dto.StockRequest;
+import lombok.RequiredArgsConstructor;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Service;
+import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+@Service
+@RequiredArgsConstructor
+public class StockCollectService {
+
+    private final WebClient webClient;
+    private final StockPublishService stockPublishService;
+
+    @Scheduled(cron = "0 0 * * * *")
+    public void collectStock() {
+        getStockData()
+            .flatMapMany(Flux::fromArray)
+            .next()
+            .subscribe(stockPublishService::publish);
+    }
+
+    private Mono<StockRequest[]> getStockData() {
+        return webClient.get()
+            .uri(uriBuilder -> uriBuilder
+                .path("/ticker")
+                .queryParam("markets", "KRW-BTC")
+                .build())
+            .retrieve()
+            .bodyToMono(StockRequest[].class);
+    }
+}
