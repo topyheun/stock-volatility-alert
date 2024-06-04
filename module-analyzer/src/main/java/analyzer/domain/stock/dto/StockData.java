24abc8e0b9789e8135536095cf5e45ab167756e0
Topy <gmsdl1994@naver.com>
2024-06-05 00:54:45 +0900

    종목 정보 분석 dto 추가

---
+++ module-analyzer/src/main/java/analyzer/domain/stock/dto/StockData.java
@@ -0,0 +1,13 @@
+package analyzer.domain.stock.dto;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+public record StockData(
+    @JsonProperty("market")
+    String ticker,
+
+    @JsonProperty("trade_price")
+    int price
+) {
+
+}
