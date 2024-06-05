69f8844dfd9e8d83942fe5773e08907c46eacb7e
Topy <gmsdl1994@naver.com>
2024-06-05 14:17:47 +0900

    slack webhook url 변경

--- module-analyzer/src/main/java/analyzer/global/common/Const.java
+++ module-analyzer/src/main/java/analyzer/global/common/Const.java
@@ -2,5 +2,5 @@ package analyzer.global.common;
 
 public final class Const {
 
-    public static final String SLACK_WEBHOOK_URL_VOLATILITY_ALERT = "https://hooks.slack.com/services/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";
+    public static final String SLACK_WEBHOOK_URL_VOLATILITY_ALERT = "https://hooks.slack.com/services/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";
 }
