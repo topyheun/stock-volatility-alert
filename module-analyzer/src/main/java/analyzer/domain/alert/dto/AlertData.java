233711900b9716c2773229f003bc6be2ae2eb2ea
Topy <gmsdl1994@naver.com>
2024-06-05 14:18:06 +0900

    종목 정보 알림 메시지 포맷 변경

--- module-analyzer/src/main/java/analyzer/domain/alert/dto/AlertData.java
+++ module-analyzer/src/main/java/analyzer/domain/alert/dto/AlertData.java
@@ -1,10 +1,15 @@
 package analyzer.domain.alert.dto;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 public record AlertData(
     String text
 ) {
 
     public static AlertData of(String message, double changePercentage) {
-        return new AlertData(String.format("[변동성 알림] 종목 : %s, 변동률 : %.2f%%", message, changePercentage));
+        LocalDateTime now = LocalDateTime.now();
+        String formattedDateTime = now.format(DateTimeFormatter.ofPattern("yyyy.MM.dd HH:mm"));
+        return new AlertData(String.format("[변동성 알림] 종목 : %s, 변동률 : %.2f%%  //  %s", message, changePercentage, formattedDateTime));
     }
 }
