d62665b0019eb29930f60df57b9dd59b7f6c77fc
Topy <gmsdl1994@naver.com>
2024-06-26 19:10:56 +0900

    build: MockWebServer 추가

--- module-collector/build.gradle.kts
+++ module-collector/build.gradle.kts
@@ -1,4 +1,7 @@
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter-webflux")
     implementation("org.springframework.boot:spring-boot-starter-data-redis")
+
+    // MockWebServer for testing
+    testImplementation("com.squareup.okhttp3:mockwebserver:4.12.0")
 }
